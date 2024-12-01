65cf6f713f7e86dc36be8d8f3bf10e93d27ff772
dawidzawislak <zawislak.dawid02@gmail.com>
2024-12-01 15:36:59 +0100

    adjusted times for batch script

--- run.bat
+++ run.bat
@@ -4,8 +4,10 @@ start /wait cmd /c "cd server && pip install -r requirements.txt && cd ..\app &&
 
 start cmd /k "cd server && python main.py"
 
+timeout /t 3 /nobreak
+
 start cmd /k "cd app && npm run dev"
 
-timeout /t 2 /nobreak
+timeout /t 3 /nobreak
 
 start http://localhost:5173/
